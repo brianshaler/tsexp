30a78c7649b320cb82932e8426b5f293857e00ac
Brian Shaler <brianshaler@gmail.com>
2023-01-31 07:39:13 -0700

    add ts boilerplate

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Info } from './types';
+
+export const info: Info = {
+  hello: 'World',
+  version: {
+    versionString: '1.0.0',
+    major: '1',
+    minor: '0',
+    patch: '0',
+  },
+};
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type Version = {
+  versionString: '1.0.0';
+  major: '1';
+  minor: '0';
+  patch: '0';
+};
+
+export type Info = {
+  hello: 'World';
+  version: Version;
+};
